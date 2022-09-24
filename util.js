8db845861961e3a240dbb643a18154a92c63e43c
Benton Li <cl2597@cornell.edu>
2022-09-23 23:22:18 -0400

    Update util.js

--- util.js
+++ util.js
@@ -1,1 +1,20 @@
 exports.get_time = () => Math.floor(Date.now() / 1000);
+
+exports.top_three = (arr) => {
+  var fst = 0;
+  var scd = 0;
+  var thd = 0;
+
+  for (i of arr)
+    if (i > fst) {
+      thd = scd;
+      scd = fst;
+      fst = i;
+    } else if (i > scd) {
+      thd = scd;
+      scd = i;
+    } else if (i > thd) {
+      thd = i;
+    }
+  return [fst, scd, thd];
+};
