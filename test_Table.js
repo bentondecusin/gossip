2770e1ef5a13c278c2598063fa7948974fc8ead5
Benton Li <cl2597@cornell.edu>
2022-09-26 16:33:55 -0400

    Create test_Table.js

---
+++ test_Table.js
@@ -0,0 +1,15 @@
+const { Table } = require("./Table.js");
+const { Entry } = require("./Entry.js");
+
+let t = new Table();
+t.add_entry("42.42.42.42", "2333", 103, 2);
+t.add_entry("42.42.42.42", "2334", 104, 2);
+t.add_entry("42.42.42.42", "2335", 105, 2);
+t.add_entry("42.42.42.42", "2336", 106, 2);
+t.add_entry("42.42.42.42", "2333", 103, 2);
+t.add_entry("42.42.42.42", "2333", 103, 2);
+
+t.pretty_print();
+
+let e = new Entry("2333", 100, 2);
+e.pretty_print();
