54e014d1ed419fd31d83018812e398bafaaa2ee7
Benton Li <cl2597@cornell.edu>
2022-09-26 20:29:38 -0400

    Update entry and add test cases

--- Entry.js
+++ Entry.js
@@ -1,32 +1,41 @@
 module.exports.Entry = class Entry {
   port;
   entry_arr = []; // entry_arr: idx -> entry_arr[i] = [port, ts, digit]
-  oldest_ts = 6969696969;
-  oldest_idx = 0;
 
   constructor(port, ts, digit) {
     this.entry_arr = [[port, ts, digit]];
-    this.oldest_ts = ts;
   }
 
   pretty_print = () => console.log(this.entry_arr);
   add_entry = (port, ts, digit) => {
-    if (this.entry_arr.length < 3 && ts < this.oldest_ts) {
-      this.entry_arr.push([port, ts, digit]);
-      this.oldest_ts = ts;
-      this.oldest_idx = entry_arr.length - 1;
-    } else if (this.entry_arr.length < 3)
-      this.entry_arr.push([port, ts, digit]);
-    else if (ts < this.oldest_ts) return;
-    else {
-      this.oldest_ts = 6969696969;
-      this.entry_arr[this.oldest_idx] = [port, ts, digit];
+    // If empty, insert
+    if (this.entry_arr.length == 0) this.entry_arr = [[port, ts, digit]];
 
-      for (let i = 0; i < this.entry_arr.length; i++)
-        if (this.entry_arr[i][1] <= this.oldest_ts) {
-          this.oldest_ts = this.entry_arr[i][1];
-          this.oldest_idx = i;
+    // Check if port exists
+    for (var i in this.entry_arr)
+      if (this.entry_arr[i][0] === port) {
+        if (this.entry_arr[i][1] <= ts) {
+          // Check if entry ts is older than the current one
+          this.entry_arr[i][2] = digit;
+          this.entry_arr[i][1] = ts;
         }
+        return;
+      }
+    // If not exists, add to entry_arr if there's space
+    if (this.entry_arr.length < 3) this.entry_arr.push([port, ts, digit]);
+    // Otherwise kick out the oldest one if older than ts
+    else {
+      var oldest_ts = 6969696969;
+      var oldest_idx = 0;
+      // Find the oldest entry's index
+      for (var i in this.entry_arr)
+        if (this.entry_arr[i][1] < oldest_ts) {
+          oldest_ts = this.entry_arr[i][1];
+          oldest_idx = i;
+        }
+
+      // If older, then kick out the oldest
+      if (oldest_ts < ts) this.entry_arr[oldest_idx] = [port, ts, digit];
     }
   };
 };
--- test_Table.js
+++ test_Table.js
@@ -8,8 +8,9 @@ t.add_entry("42.42.42.42", "2335", 105, 2);
 t.add_entry("42.42.42.42", "2336", 106, 2);
 t.add_entry("42.42.42.42", "2333", 103, 2);
 t.add_entry("42.42.42.42", "2333", 103, 2);
+t.add_entry("42.42.42.42", "2336", 109, 6);
 
 t.pretty_print();
 
 let e = new Entry("2333", 100, 2);
-e.pretty_print();
+//e.pretty_print();
